066001ae105fd8f48a7906b49e30ba2adfe7212e
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-06-14 11:43:19 +0330

    feat: Added TimeStampMixin for avoid code duplication.

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext as _
 from uuid import uuid4
 
 
@@ -11,3 +12,14 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class TimeStampMixin:
+    created_at = models.DateTimeField(
+        verbose_name=_('created at'),
+        auto_now_add=True,
+    )
+    update_at = models.DateTimeField(
+        verbose_name=_('updated at'),
+        auto_now=True,
+    )
