01dbfa48646fb4c75f303249738a456aaf8a18eb
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-06-14 11:38:33 +0330

    feat: Added UUID field fo set to all id of models.

--- core/models.py
+++ core/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from uuid import uuid4
 
 
 class BaseModel(models.Model):
-    ...
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid4,
+        editable=False,
+    )
 
     class Meta:
         abstract = True
