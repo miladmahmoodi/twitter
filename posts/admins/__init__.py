a737041b894e7e70a57b4af7ee8dfbebbdf6019d
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-06-18 13:21:26 +0330

    feat: Implemented admins package and base admins file.

---
+++ posts/admins/__init__.py
@@ -0,0 +1,5 @@
+from .comment import CommentAdmin
+from .image import ImageAdmin
+from .like import LikeAdmin
+from .post import PostAdmin
+from .tag import TagAdmin
---
+++ posts/admins/image.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from ..models import Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    """
+
+    """
+    pass
