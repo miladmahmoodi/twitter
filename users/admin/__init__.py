6bbb8913eed29944e98273510906c785e68a0fd2
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-06-19 13:20:31 +0330

    fix: Created init file to change directory to package and fix UserAdmin.

---
+++ users/admin/__init__.py
@@ -0,0 +1,2 @@
+from .user import UserAdmin
+from .relation import RelationAdmin
--- users/admin/user.py
+++ users/admin/user.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from ..models import User
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     """
 
     """
 
-    pass
+    list_filter = ['username']
+
+    filter_horizontal = ()
+
