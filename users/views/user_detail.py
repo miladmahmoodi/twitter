42b9026aea6d1660f70e63b35626b5ba1399ece9
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-06-20 11:12:02 +0330

    build: Builded base of user_detail view.

--- users/views/user_detail.py
+++ users/views/user_detail.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-def user_detail(request):
+def user_detail(request, username):
     """
 
     :param request:
+    :param username:
     :return:
     """
-    pass
+    user = User.objects.get(username=username)
+    return HttpResponse(user.get_full_name())
