7f89825e09951c35df5995f2d985b1805161545f
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-06-27 14:39:16 +0330

    feat: Implemented signin view.

---
+++ users/views/sign_in.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+
+def signin_view(request):
+    """
+
+    :param request:
+    :return:
+    """
+
+    if request.method == 'GET':
+        """
+        """
+        if request.user.is_authenticated:
+            return redirect(
+                '/home/'
+            )
+
+        return render(
+            request,
+            template_name='users/sign-in.html',
+        )
+
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            user = authenticate(
+                request,
+                username=request.POST.get('username'),
+                password=request.POST.get('password'),
+            )
+            if user:
+                login(
+                    request,
+                    user,
+                )
+
+            return redirect(
+                f'/{user.username}',
+            )
+        return redirect(
+            '/home/'
+        )
