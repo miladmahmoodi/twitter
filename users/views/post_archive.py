088c2b6bb67bb81590e6aa052ee3b2ea07eaa2ed
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-06-27 17:58:28 +0330

    feat: Implemented post_archive_view

--- users/views/post_archive.py
+++ users/views/post_archive.py
@@ -1,2 +1,20 @@
-def post_archive_view(request):
-    pass
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import get_user_model
+
+from posts.models import Post, Comment
+from users.models import User
+
+
+def post_archive_view(request, username, post_id):
+
+    if request.method == 'GET':
+        post = get_object_or_404(
+            Post,
+            id=post_id,
+        )
+        if request.user == post.user:
+            post.delete()
+
+        return redirect(
+            f'/{post.user}/'
+        )
