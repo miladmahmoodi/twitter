2fced47574ebd878df1460978e91a0c519237d84
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-07-12 09:49:31 +0330

    fix: Fixed create post view.

--- posts/views/post_create.py
+++ posts/views/post_create.py
@@ -1,8 +1,11 @@
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
-from posts.forms import PostCreateForm, ImageCreateFormSet, ImageCreateForm
+from posts.forms import PostCreateForm, ImageCreateFormSet
+from posts.models import Image, Tag
+from core.utils import UsersMessages
 
 
 class PostCreateView(LoginRequiredMixin, View):
@@ -21,15 +24,9 @@ class PostCreateView(LoginRequiredMixin, View):
         :return:
         """
 
-        inlines = (
-            ImageCreateFormSet(),
-        )
-
-        form = PostCreateForm()
-        form.inlines = inlines
-
         context = {
-            'form': form,
+            'form': self.form_class(),
+            'image_form': self.image_form_set(),
         }
         return render(
             request,
@@ -39,12 +36,44 @@ class PostCreateView(LoginRequiredMixin, View):
 
     def post(self, request):
         form = self.form_class(request.POST)
-        image_formset = self.image_form_class(request.POST, request.FILES)
-        if form.is_valid() and image_formset.is_valid():
-            post = form.save(commit=False)
+        image_form = self.image_form_set(request.POST, request.FILES)
+        if form.is_valid() and image_form.is_valid():
+            post = form.save(
+                commit=False,
+            )
             post.user = request.user
             post.save()
-            image_formset.instance = post
-            image_formset.save()
-            return redirect('post:explore')
-        return redirect('post:create_post')
+            for tag in form.cleaned_data.get('tags').split(' '):
+                obj, created = Tag.objects.get_or_create(
+                    name=tag,
+                )
+                post.tag.add(obj)
+
+            for image in image_form.cleaned_data:
+                if image:
+                    Image.objects.create(
+                        name=image.get('name'),
+                        alt=image.get('alt'),
+                        image=image.get('image'),
+                        post=post,
+                    )
+
+            messages.add_message(
+                request,
+                level=messages.SUCCESS,
+                message=UsersMessages.create_post_denied,
+            )
+            return redirect(
+                'user:user_detail',
+                pk=request.user.pk,
+            )
+
+        context = {
+            'form': self.form_class(),
+            'image_form': self.image_form_set(),
+        }
+        return render(
+            request,
+            template_name=self.template_name,
+            context=context,
+        )
